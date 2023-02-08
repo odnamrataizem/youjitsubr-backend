0c44581f9e9f1cfca601c0d1ee6d436428069c17
Armando Meziat <amf-commits@mezi.at>
2023-02-08 18:19:56 -0300

    Support running on non-default ports

--- config/storage.ts
+++ config/storage.ts
@@ -4,7 +4,9 @@ import { mkdirpSync } from 'mkdirp';
 const storagePath = 'public/images';
 mkdirpSync(storagePath);
 
-const host = (process.env.ASSET_BASE_URL ?? '') || 'http://localhost:3000';
+const host =
+  (process.env.ASSET_BASE_URL ?? '') ||
+  `http://localhost:${process.env.PORT ?? 3000}`;
 
 export const storage: Record<string, StorageConfig> = {
   myLocal: {
